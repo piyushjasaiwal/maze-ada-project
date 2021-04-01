6e582aaa4ecaabb01c3cf16039938bba7a591de9
piyushjasaiwal <coderpiyush123@gmail.com>
2021-04-01 21:29:28 +0530

    maze generated partially completed

--- js_files/maze_maker.js
+++ js_files/maze_maker.js
@@ -17,6 +17,31 @@ class Cell{
             left_wall : true
         };
     }
+
+    checkNeighbours(){
+        let grid = this.parent_grid;
+        let row = this.row_num;
+        let col = this.col_num;
+        let neighbours = [];
+
+        let top = row !== 0 ? grid[row-1][col] : undefined;
+        let right = col !== grid.length - 1 ? grid[row][col + 1] : undefined;
+        let bottom = row !== grid.length - 1 ? grid[row + 1][col] : undefined;
+        let left = col !== 0 ? grid[row][col - 1] : undefined;
+        
+        if(top && !top.visited) neighbours.push(top);
+        if(right && !right.visited) neighbours.push(right);
+        if(bottom && !bottom.visited) neighbours.push(bottom);
+        if(left && !left.visited) neighbours.push(left);
+
+        if(neighbours.length !== 0){
+            let random = Math.floor(Math.random() * neighbours.length);
+            return neighbours[random];
+        }else{
+            return undefined
+        }
+    }
+
     draw_top_wall(x, y, size, columns, rows){
         ctx.beginPath();
         ctx.moveTo(x, y);
@@ -45,12 +70,44 @@ class Cell{
         ctx.stroke();
     }
 
+    highlight(columns){
+        let x = (this.col_num * this.parent_size)/columns + 1
+        let y = (this.row_num * this.parent_size)/columns + 1
+        ctx.fillStyle = "purple";
+        ctx.fillRect(x, y, this.parent_size/ columns - 3, this.parentSize / columns - 3);
+    }
+
+    remove_walls(cell1, cell2){
+        let x = cell1.col_num - cell2 .col_num;
+        if(x == 1){
+            cell1.walls.left_wall = false;
+            cell2.walls.right_wall = false;
+        }
+
+        else if(x == -1){
+            cell1.walls.right_wall = false;
+            cell2.walls.left_wall = false;
+        }
+
+        let y = cell1.row_num - cell2 .row_num;
+        if(y == -1){
+            cell1.walls.bottom_wall = false;
+            cell2.walls.top_wall = false;
+        }
+
+        else if(y == 1){
+            cell1.walls.top_wall = false;
+            cell2.walls.bottom_wall = false;
+        }
+
+    }
+
     show(size, r ,c){
         let x = (this.col_num * size) / c;
         let y = (this.row_num * size) / r;
 
         ctx.strokeStyle = "white";
-        ctx.fillStyle = "black";
+        // ctx.fillStyle = "green";
         ctx.lineWidth = 2;
 
         if(this.walls.top_wall){
@@ -113,10 +170,57 @@ class Maze{
                 grid[r][c].show(this.size, this.rows, this.columns);
             }
         }
+        let next = current.checkNeighbours();
+        if(next){
+            next.visited = true;
+            this.stack.push(current);
+
+            current.highlight(this.columns);
+            
+            current.remove_walls(current, next);
+            current = next;
+        }else if(this.stack.length > 0){
+            let cell = this.stack.pop();
+            current = cell;
+            current.highlight(this.columns);
+            
+        }
+
+        if(this.stack.length == 0){
+            return ;
+        }
+
+        window.requestAnimationFrame(() => {
+            this.draw();
+        })
+    }
+
+    maze_setup(){
+        maze.width = this.size;
+        maze.height = this.size;
+
+        maze.style.background = "black";
+
+        current.visited = true;
+
+        for(let r = 0; r < this.rows;r++){
+            for(let c = 0;c <= this.columns;c++){
+                let grid = this.grid;
+                grid[r][c].show(this.size, this.rows, this.columns);
+            }
+        }
     }
 }
+let newMaze ;
+document.getElementById("grid").onclick = function(){
+    newMaze = new Maze(550, 20, 20);
+    newMaze.setup();
+    newMaze.maze_setup();
+}
 
+document.getElementById("random").onclick = function(){
+    newMaze.draw();
+}
 
-let newMaze = new Maze(500, 10, 10);
-newMaze.setup();
-newMaze.draw()
+// newMaze.maze_setup();
+// newMaze.draw();
